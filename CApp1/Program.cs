7068aafe931bc2d65ada3b12e144ad25cf4f5e7e
PavelM <reginuser@gmail.com>
2017-10-16 22:12:46 +0300

    2

--- CApp1/Program.cs
+++ CApp1/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Eg.Core;
+using NHibernate.Cfg;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Reflection;
+
+//[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace CApp1
 {
@@ -10,6 +13,36 @@ namespace CApp1
     {
         static void Main(string[] args)
         {
+            log4net.Config.XmlConfigurator.Configure();
+
+            var nhConfig = new Configuration().Configure();
+
+            nhConfig.AddAssembly(typeof(Product).Assembly);
+
+            var sessionFactory = nhConfig.BuildSessionFactory();
+
+            var update = new SchemaUpdate(nhConfig);
+            update.Execute(false, true);
+
+            using (var session = sessionFactory.OpenSession())
+            {
+                using (var tr = session.BeginTransaction())
+                {
+                    var prod = new Product() {                        
+                        Name = "Pepsi4",
+                        Description = "Beverage",
+                        UnitPrice = 1.15M
+                    };
+
+                    session.Save(prod);
+
+                    tr.Commit();
+                }
+            }
+            
+
+            Console.WriteLine("Finished!");
+            Console.ReadLine();
         }
     }
 }
